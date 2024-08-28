fb411ea889dffb00a6c91f2158df01e968508c10
cyanf1sh <yuqiuchu19@mails.ucas.ac.cn>
2024-08-28 06:24:20 +0000

    build: merge code

---
+++ python/ailang/nn/layers/__init__.py
@@ -0,0 +1,2 @@
+from ailang.nn.layers.base import Module
+from ailang.nn.layers.convolution import Conv2d
---
+++ python/ailang/nn/layers/activations.py
@@ -0,0 +1,24 @@
+import math
+from typing import Any
+
+import ailang as al
+from ailang.nn.layers.base import Module
+
+
+def _make_activation_module(f):
+    def decorator(klass):
+        klass.__doc__ = f.__doc__
+        klass.__call__ = lambda self, x: f(x)
+        return klass
+
+    return decorator
+
+
+def sigmoid(x):
+    r"""Applies the element-wise function:
+
+    .. math::
+        \text{Sigmoid}(x) = \sigma(x) = \frac{1}{1 + \exp(-x)}
+    """
+
+    return al.sigmoid(x)
