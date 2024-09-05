0c0ce54bcc918c962f166a42cf4c5a0cb02f9516
cyanf1sh <yuqiuchu19@mails.ucas.ac.cn>
2024-09-05 09:39:16 +0000

    enhance: add lstm per-operator jit test.

--- python/ailang/nn/layers/lstmcell.py
+++ python/ailang/nn/layers/lstmcell.py
@@ -8,22 +8,26 @@ class LSTMCell(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
 
-        self.weight_ih = al.random.randn((4 * hidden_size, input_size), al.f32)
-        self.weight_hh = al.random.randn((4 * hidden_size, hidden_size), al.f32)
-        self.bias = al.random.randn((4 * hidden_size), al.f32)
+        self.wf = al.random.randn(hidden_size, input_size + hidden_size, dtype=al.f32)
+        self.bf = al.random.randn(hidden_size, 1, dtype=al.f32)
+        self.wi = al.random.randn(hidden_size, input_size + hidden_size, dtype=al.f32)
+        self.bi = al.random.randn(hidden_size, 1, dtype=al.f32)
+        self.wc = al.random.randn(hidden_size, input_size + hidden_size, dtype=al.f32)
+        self.bc = al.random.randn(hidden_size, 1, dtype=al.f32)
+        self.wo = al.random.randn(hidden_size, input_size + hidden_size, dtype=al.f32)
+        self.bo = al.random.randn(hidden_size, 1, dtype=al.f32)
 
-    def __call__(self, x: al.array, hx: al.array, cx: al.array) -> al.array:
-        gates = (
-            al.matmul(x, self.weight_ih.T) + al.matmul(hx, self.weight_hh.T) + self.bias
+    def sigmoid(self, x):
+        return al.standard.div(
+            1.0, al.standard.add(1.0, al.standard.exp(al.standard.neg(x)))
         )
-        i, f, g, o = al.split(gates, 4, axis=1)
 
-        i = al.sigmoid(i)
-        f = al.sigmoid(f)
-        g = al.tanh(g)
-        o = al.sigmoid(o)
-
-        cy = f * cx + i * g
-        hy = o * al.tanh(cy)
-
-        return hy, cy
+    def forward(self, x_t, h_prev, c_prev):
+        x = al.standard.cat([h_prev, x_t], axis=0)
+        f = self.sigmoid(al.standard.add(al.matmul(self.wf, x), self.bf))
+        i = self.sigmoid(al.standard.add(al.matmul(self.wi, x), self.bi))
+        c_hat = al.standard.tanh(al.standard.add(al.matmul(self.wc, x), self.bc))
+        c = al.standard.add(al.standard.mul(f, c_prev), al.standard.mul(i, c_hat))
+        o = self.sigmoid(al.standard.add(al.matmul(self.wo, x), self.bo))
+        h = al.standard.mul(o, al.standard.tanh(c))
+        return h, c
