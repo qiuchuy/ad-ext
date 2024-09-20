fix resnet with precision 1e-4 / 1e-6

python/ailang/nn/layers/normalization.py
@@ -28,8 +28,8 @@ class Batchnorm2d(Module):
         np_offset = np.zeros((num_features), dtype=np.float32)
         self.offset = al.from_numpy(np_offset)
         self.scale = al.from_numpy(np_scale)
-        self.running_mean = None
-        self.running_var = None
+        self.running_mean = np.zeros((self.num_features), dtype=np.float32)
+        self.running_var = np.ones((self.num_features), dtype=np.float32)
 
     def compute_mean(self, x: al.array):
         return al.standard.mean(x, [0, 2, 3])
@@ -57,7 +57,6 @@ class Batchnorm2d(Module):
             self.running_var = al.from_numpy(
                 np.ones((self.num_features), dtype=np.float32)
             )
-        print(self.running_mean)
         return al.standard.batchnorm2d(
             x, self.scale, self.offset, self.running_mean, self.running_var
         )

python/ailang/nn/layers/pooling.py
@@ -20,7 +20,7 @@ class Maxpool2d(Module):
             (kernel_size, stride, base_dilations, window_dilations),
         )
         (padding,) = map(
-            lambda x: (0, 0, 0, 0, 0, 0, x, x) if isinstance(x, int) else x, (padding,)
+            lambda x: (0, 0, 0, 0, x, x, x, x) if isinstance(x, int) else x, (padding,)
         )
         self.kernel_size = kernel_size
         self.padding = padding
@@ -37,6 +37,48 @@ class Maxpool2d(Module):
             len(input.shape) == 4
         ), "[nn.Maxpool2d] input's dims expected to be (N,C,H,W)"
         y = al.standard.maxpool2d(
+            input,
+            self.kernel_size,
+            self.stride,
+            self.base_dilations,
+            self.window_dilations,
+            self.padding,
+        )
+        return y
+
+
+class Avgpool2d(Module):
+    def __init__(
+        self,
+        kernel_size: Union[int, tuple] = 3,
+        stride: Union[int, tuple] = 1,
+        base_dilations: Union[int, tuple] = 1,
+        window_dilations: Union[int, tuple] = 1,
+        padding: Union[int, tuple] = 0,
+    ):
+        super().__init__()
+        kernel_size, stride, base_dilations, window_dilations = map(
+            lambda x: (1, 1, x, x) if isinstance(x, int) else x,
+            (kernel_size, stride, base_dilations, window_dilations),
+        )
+        (padding,) = map(
+            lambda x: (0, 0, 0, 0, x, x, x, x) if isinstance(x, int) else x, (padding,)
+        )
+        self.kernel_size = kernel_size
+        self.padding = padding
+        self.stride = stride
+        self.base_dilations = base_dilations
+        self.window_dilations = window_dilations
+
+    def get_random_array(self, shape: Tuple[int], dtype: np.dtype):
+        np_array = np.random.randn(*shape).astype(dtype)
+        return al.from_numpy(np_array)
+
+    def __call__(self, input):
+        assert (
+            len(input.shape) == 4
+        ), "[nn.Avgpool2d] input's dims expected to be (N,C,H,W)"
+        y = al.standard.avgpool2d(
             input,
             self.kernel_size,
             self.stride,

tests/test_nn.py
@@ -2,6 +2,7 @@ import numpy as np
 
 np.random.seed(42)
 
+
 class LSTMCell:
     def __init__(self, input_size, hidden_size):
         self.input_size = input_size
@@ -136,6 +137,7 @@ b_ho = b_hh[3 * hidden_size :]
 
 import ailang as al
 
+
 class ALLSTMCell(nn.Module):
     def __init__(self, input_size: int, hidden_size: int) -> None:
         super().__init__()
@@ -216,9 +218,3 @@ assert np.allclose(
     rtol=1e-4,
     atol=1e-6,
 )
-
-
-
-
-
-